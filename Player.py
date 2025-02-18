50cbb57cefea6c87036121e61aa827f1647ae8b0
cutejunkie <obrzud.gabriela@o2.pl>
2025-02-18 23:01:06 +0100

    Flies and eating flies added

--- Player.py
+++ Player.py
@@ -3,45 +3,46 @@ from config import window, window_width, window_height
 
 class Player:
     def __init__(self):
+        self.speed = 3
         self.x_cord = 0
         self.y_cord = 0
-        self.p_width = 0
-        self.p_height = 0
         self.image = pygame.image.load("./images/player.png")
+        self.p_width, self.p_height = self.image.get_size()
+        self.hitbox = pygame.Rect(self.x_cord, self.y_cord, self.p_width, self.p_height)
+
 
     # once per every while loop
     def tick(self):
-        self.p_width, self.p_height = self.image.get_size()
-
         # keys clicked
         keys = pygame.key.get_pressed()
 
-        speed = 3
-
         # arrows clicked /// WSAD clicked
         if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-            if self.x_cord + speed < window_width - self.p_width:
-                self.x_cord += speed
+            if self.x_cord + self.speed < window_width - self.p_width:
+                self.x_cord += self.speed
             else:
                 self.x_cord = window_width - self.p_width
 
         if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-            if self.x_cord - speed < 0:
+            if self.x_cord - self.speed < 0:
                 self.x_cord = 0
             else:
-                self.x_cord -= speed
+                self.x_cord -= self.speed
 
         if keys[pygame.K_UP] or keys[pygame.K_w]:
-            if self.y_cord - speed < 0:
+            if self.y_cord - self.speed < 0:
                 self.y_cord = 0
             else:
-                self.y_cord -= speed
+                self.y_cord -= self.speed
 
         if keys[pygame.K_DOWN] or keys[pygame.K_s]:
-            if self.y_cord + speed > window_height - self.p_height:
+            if self.y_cord + self.speed > window_height - self.p_height:
                 self.y_cord = window_height - self.p_height
             else:
-                self.y_cord += speed
+                self.y_cord += self.speed
+
+        # refreshing hitbox
+        self.hitbox = pygame.Rect(self.x_cord, self.y_cord, self.p_width, self.p_height)
 
     # showing player in window
     def draw(self):
--- main.py
+++ main.py
@@ -1,24 +1,45 @@
 import pygame
 from config import window
 from Player import Player
+from Fly import Fly
 
 def main():
     run = True
+    score = 0
     player = Player()
+    clock = 0  # sth like stopper
+    allflies = []
+
     while run:
-        pygame.time.Clock().tick(60)  # max 60 fps
+        clock += pygame.time.Clock().tick(60) / 1000 # max 60 fps /// %1000 to get seconds
         for event in pygame.event.get():
             if event.type == pygame.QUIT:  # closing by X
                 run = False
 
+        if clock >= 3:
+            clock = 0
+            allflies.append(Fly())
+
         player.tick()
+        for fly in allflies:
+            fly.tick()
+
+        # player ate fly
+        for fly in allflies:
+            if player.hitbox.colliderect(fly.hitbox):
+                allflies.remove(fly)
+                score += 1
 
         # background
         window.fill((247, 198, 223))
-        # player
         player.draw()
+        for fly in allflies:
+            fly.draw()
 
         pygame.display.update()
+
+
+    print(score)
 
 if __name__ == "__main__":
     main()
